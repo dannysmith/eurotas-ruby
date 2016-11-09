b2fab230f1a0c9922dbfd53e6f2a13a3794feefd
Danny Smith <hi@danny.is>
2016-11-09 13:08:12 +0000

    Test Commit

--- eurotas.rb
+++ eurotas.rb
@@ -97,7 +97,7 @@ module Eurotas
         match = path.match params[:folder_map]
         if match
           dest_path = File.join(params[:destination], match.captures)
-          commands << "mkdir -p #{dest_path} && cp -rf #{path} #{dest_path}"
+          commands << "mkdir -p #{dest_path} && cp -rf #{path}. #{dest_path}"
         end
       end
       commands
